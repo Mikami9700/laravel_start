17cf46dd5c717166b79e95c82ee15d8a74699d12
Mikami <Mikami9700@users.noreply.github.com>
2020-09-19 15:00:30 +0300

    15

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Front\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +24,22 @@ Route::get('/show-number/{id}', function ($id) {
 Route::get('show-string/{id?}', function ($id) {
     return $id;
 })->name('b');
+
+// NameSpace
+Route::namespace('Front')->group(function() {
+
+    // All route only access controller or methods in folder name Front 
+    // Route::get('users', 'UserController@showUserName');
+    Route::get('users/show', [UserController::class, 'showUserName']);
+});
+// Group = prefix + middleware
+Route::group(['prefix' => 'users', 'middleware' => 'auth'], function() {
+    Route::get('/info', function() {
+        return 'User INFO';
+    });
+});
+
+ // MiddlWare
+Route::get('middleware', function() {
+    return "MiddleWare";
+})->middleware('auth');
